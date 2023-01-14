d6be3520a5bf5599c0202e751f1e6525e92feebf
SirLibertino <99144376+SirLibertino@users.noreply.github.com>
2023-01-14 04:22:12 +0100

    Update KAROOTI.sol

--- KAROOTI.sol
+++ KAROOTI.sol
@@ -9,65 +9,79 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract KAROOTI is ERC20, ERC20Burnable, Ownable, ReentrancyGuard {
-  using SafeMath for uint256;
+    using SafeMath for uint256;
 
-  mapping(address => uint256) private _balances;
-  mapping(address => bool) controllers;
+    mapping(address => uint256) private _balances;
+    mapping(address => bool) controllers;
 
-  uint256 private _totalSupply;
-  uint256 private MAXSUP;
-  uint256 constant MAXIMUMSUPPLY=117000000000*10**18;
+    uint256 private _totalSupply;
+    uint256 private MAXSUP;
+    uint256 constant MAXIMUMSUPPLY = 117000000000 * 10**18;
 
-  constructor() ERC20("KAROOTI", "WHY") { 
-      _mint(msg.sender, 17000000000 * 10 ** 18);
+    constructor() ERC20("KAROOTI", "WHY") {
+        _mint(msg.sender, 17000000000 * 10**18);
+    }
 
-  }
+    function mint(address to, uint256 amount) external nonReentrant {
+        require(controllers[msg.sender], "Only controllers can mint");
+        require(
+            (MAXSUP + amount) <= MAXIMUMSUPPLY,
+            "Maximum supply has been reached"
+        );
+        _totalSupply = _totalSupply.add(amount);
+        MAXSUP = MAXSUP.add(amount);
+        _balances[to] = _balances[to].add(amount);
+        _mint(to, amount);
+    }
 
-  function mint(address to, uint256 amount) external nonReentrant {
-    require(controllers[msg.sender], "Only controllers can mint");
-    require((MAXSUP+amount)<=MAXIMUMSUPPLY,"Maximum supply has been reached");
-    _totalSupply = _totalSupply.add(amount);
-    MAXSUP=MAXSUP.add(amount);
-    _balances[to] = _balances[to].add(amount);
-    _mint(to, amount);
-  }
+    function burnFrom(address account, uint256 amount)
+        public
+        override
+        nonReentrant
+    {
+        if (controllers[msg.sender]) {
+            _burn(account, amount);
+        } else {
+            super.burnFrom(account, amount);
+        }
+    }
 
-  function burnFrom(address account, uint256 amount) public override nonReentrant {
-      if (controllers[msg.sender]) {
-          _burn(account, amount);
-      }
-      else {
-          super.burnFrom(account, amount);
-      }
-  }
+    function transfer(address to, uint256 value)
+        public
+        override
+        nonReentrant
+        returns (bool)
+    {
+        require(balanceOf(msg.sender) >= value, "Insufficient balance");
+        // Get the current balance of the sender
+        uint256 senderBalance = _balances[msg.sender];
+        // Subtract the value from the sender's balance
+        senderBalance = senderBalance.sub(value);
+        // Update the balance of the sender
+        _balances[msg.sender] = senderBalance;
+        // Add the value to the balance of the recipient
+        _balances[to] = _balances[to].add(value);
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
 
-  function transfer(address to, uint256 value) public override nonReentrant returns (bool) {
-    require(balanceOf(msg.sender) >= value, "Insufficient balance");
+    function addController(address controller) external onlyOwner nonReentrant {
+        controllers[controller] = true;
+    }
 
-  // transfer the tokens and update the contract's state
-  balanceOf[msg.sender] -= value;
-  balanceOf[to] += value;
+    function removeController(address controller)
+        external
+        onlyOwner
+        nonReentrant
+    {
+        controllers[controller] = false;
+    }
 
-  // emit a Transfer event
-  emit Transfer(msg.sender, to, value);
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
+    }
 
-  return true;
-}
-
-  function addController(address controller) external onlyOwner nonReentrant {
-    controllers[controller] = true;
-  }
-
-  function removeController(address controller) external onlyOwner nonReentrant {
-    controllers[controller] = false;
-  }
-  
-  function totalSupply() public override view nonReentrant returns (uint256) {
-    return _totalSupply;
-  }
-
-  function maxSupply() public pure nonReentrant returns (uint256) {
-    return MAXIMUMSUPPLY;
-  }
-
+    function maxSupply() public pure returns (uint256) {
+        return MAXIMUMSUPPLY;
+    }
 }
